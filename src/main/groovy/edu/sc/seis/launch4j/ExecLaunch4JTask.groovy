64198a05c0a939ab8d5bb00faa260c69c20831bb
Philip Crotwell <crotwell@seis.sc.edu>
2012-01-25 11:42:16 -0500

    remove debugging outputs

--- src/main/groovy/edu/sc/seis/launch4j/ExecLaunch4JTask.groovy
+++ src/main/groovy/edu/sc/seis/launch4j/ExecLaunch4JTask.groovy
@@ -10,20 +10,11 @@ class ExecLaunch4JTask extends DefaultTask {
     @TaskAction
     def void runLaunch4J() {
         Launch4jPluginExtension configuration = project.launch4j
-        println "aaa"
-        println configuration.getXmlOutFileForProject(project).parentFile.class
-        println "aaa2"
-        println configuration.getXmlOutFileForProject(project).parentFile
-        println "bbb"
-        println project.launch4j.launch4jCmd
-        println "ccc"
-        println project.launch4j.xmlFileName
-        println "ddd"
         Process procEcho = [project.launch4j.launch4jCmd, configuration.getXmlOutFileForProject(project).getCanonicalPath()].execute(null, configuration.getXmlOutFileForProject(project).parentFile)
         procEcho.consumeProcessErrorStream(System.err)
         procEcho.consumeProcessOutputStream(System.out)
         if (procEcho.waitFor() != 0) {
-            throw new RuntimeException('exec failed')
+            throw new RuntimeException(project.launch4j.launch4jCmd+' exec failed')
         }
         println ""
     }
