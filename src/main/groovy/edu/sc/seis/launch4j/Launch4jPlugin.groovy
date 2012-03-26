a3fdfe3261b60f9af7925cc0f32cc455406c18f0
Philip Crotwell <crotwell@seis.sc.edu>
2012-03-26 17:46:44 -0400

    use lazy GStrings instead of doFirst

--- src/main/groovy/edu/sc/seis/launch4j/Launch4jPlugin.groovy
+++ src/main/groovy/edu/sc/seis/launch4j/Launch4jPlugin.groovy
@@ -41,11 +41,10 @@ class Launch4jPlugin implements Plugin<Project> {
         Task task = project.tasks.add(TASK_XML_GENERATE_NAME, CreateLaunch4jXMLTask)
         task.description = "Creates XML configuration file used by launch4j to create an windows exe."
         task.group = LAUNCH4J_GROUP
-        task.doFirst {
-            inputs.property("project version", { project.version })
-            inputs.property("Launch4j extension", {project.launch4j})
-            outputs.file(project.file(project.launch4j.xmlFileName))
-        }
+        task.inputs.property("project version", ${->project.version })
+        task.inputs.property("Launch4j extension", {project.launch4j})
+        task.outputs.file(project.file(project.launch4j.xmlFileName))
+        
         return task
     }
 
@@ -62,20 +61,18 @@ class Launch4jPlugin implements Plugin<Project> {
 
     private Task addRunLauch4jTask(Project project) {
         def task = project.tasks.add(TASK_RUN_NAME, Exec)
-        task.doFirst {
-            description = "Runs launch4j to generate an .exe file"
-            group = LAUNCH4J_GROUP
-            commandLine "$project.launch4j.launch4jCmd", "${project.buildDir}/${project.launch4j.outputDir}/${project.launch4j.xmlFileName}" 
-            workingDir "${project.buildDir}/${project.launch4j.outputDir}" 
-        }
+        task.description = "Runs launch4j to generate an .exe file"
+        task.group = LAUNCH4J_GROUP
+        task.commandLine "${->project.launch4j.launch4jCmd}", "${->project.buildDir}/${->project.launch4j.outputDir}/${->project.launch4j.xmlFileName}" 
+        task.workingDir "${->project.buildDir}/${->project.launch4j.outputDir}" 
         return task
     }
     
     private Task addLaunch4jTask(Project project) {
-        def run = project.tasks.add(TASK_LAUNCH4J_NAME)
-        run.description = "Placeholder task for tasks relating to creating .exe applications with launch4j"
-        run.group = LAUNCH4J_GROUP
-        return run
+        def task = project.tasks.add(TASK_LAUNCH4J_NAME)
+        task.description = "Placeholder task for tasks relating to creating .exe applications with launch4j"
+        task.group = LAUNCH4J_GROUP
+        return task
     }
 
     private CopySpec configureDistSpec(Project project) {
