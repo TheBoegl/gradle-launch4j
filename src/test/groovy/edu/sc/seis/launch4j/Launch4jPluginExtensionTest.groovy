d5dfd81d86707a475df7d1c12521624810961556
Sebastian Bögl <info@sebastianboegl.de>
2015-10-27 02:14:01 +0100

    add test case for a wrapped executable jar

--- src/test/groovy/edu/sc/seis/launch4j/Launch4jPluginExtensionTest.groovy
+++ src/test/groovy/edu/sc/seis/launch4j/Launch4jPluginExtensionTest.groovy
@@ -253,4 +253,76 @@ class Launch4jPluginExtensionTest extends Specification {
         process.in.text.trim().equals('Hello STDOUT!')
         process.err.text.trim().endsWith('Hello LOG!')
     }
+
+    def 'Running an unwrapped executable jar as executable with Java dependencies succeeds'() {
+        given:
+        buildFile << """
+            plugins {
+                id 'java'
+                id 'edu.sc.seis.launch4j'
+            }
+
+            repositories {
+                mavenCentral()
+            }
+
+            dependencies {
+                compile 'org.slf4j:slf4j-api:1.7.12'
+                runtime 'org.slf4j:slf4j-simple:1.7.12'
+            }
+
+            ext {
+                mainTestClassName = 'com.test.app.Main'
+            }
+
+            jar {
+                manifest {
+                    attributes 'Main-Class': mainTestClassName
+                    attributes 'Class-Path': configurations.runtime.collect { it.getName() }.join(' ')
+                }
+            }
+
+            launch4j {
+                outfile = 'test.exe'
+                dontWrapJar = true
+            }
+        """
+
+        File sourceFile = new File(testProjectDir.newFolder('src', 'main', 'java'), 'Main.java')
+        sourceFile << """
+            package com.test.app;
+
+            import org.slf4j.Logger;
+            import org.slf4j.LoggerFactory;
+
+            public class Main {
+                private static final Logger LOG = LoggerFactory.getLogger(Main.class);
+
+                public static void main(String[] args) {
+                    System.out.println("Hello STDOUT!");
+                    LOG.info("Hello LOG!");
+                }
+            }
+        """
+
+        when:
+        def result = GradleRunner.create()
+                .withProjectDir(testProjectDir.root)
+                .withArguments('-q', 'jar', 'launch4j')
+                .withPluginClasspath(pluginClasspath)
+                .build()
+
+        then:
+        result.task(':jar').outcome == SUCCESS
+        result.task(':copyL4jLib').outcome == SUCCESS
+        result.task(':launch4j').outcome == SUCCESS
+
+        def outfile = new File(testProjectDir.root, 'build/launch4j/test.exe')
+        outfile.exists()
+
+        def process = outfile.path.execute()
+        process.waitFor() == 0
+        process.in.text.trim().equals('Hello STDOUT!')
+        process.err.text.trim().endsWith('Hello LOG!')
+    }
 }
