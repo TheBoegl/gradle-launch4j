55984938fdf0fa1a35a36d3b0e7e57dc88c5fe41
Sebastian Bögl <info@sebastianboegl.de>
2016-11-23 02:08:29 +0100

    add documentation

--- src/main/groovy/edu/sc/seis/launch4j/tasks/DefaultLaunch4jTask.groovy
+++ src/main/groovy/edu/sc/seis/launch4j/tasks/DefaultLaunch4jTask.groovy
@@ -1,9 +1,6 @@
 package edu.sc.seis.launch4j.tasks
 
-import edu.sc.seis.launch4j.CopyLibraries
-import edu.sc.seis.launch4j.CreateXML
-import edu.sc.seis.launch4j.Launch4jConfiguration
-import edu.sc.seis.launch4j.Launch4jPluginExtension
+import edu.sc.seis.launch4j.*
 import org.gradle.api.DefaultTask
 import org.gradle.api.JavaVersion
 import org.gradle.api.plugins.JavaPlugin
@@ -11,7 +8,7 @@ import org.gradle.api.tasks.*
 
 abstract class DefaultLaunch4jTask extends DefaultTask implements Launch4jConfiguration {
 
-    Launch4jPluginExtension config
+    private Launch4jPluginExtension config
 
     protected DefaultLaunch4jTask() {
         this.config = project.launch4j
@@ -40,30 +37,38 @@ abstract class DefaultLaunch4jTask extends DefaultTask implements Launch4jConfig
         project.file("${project.buildDir}/${outputDir ?: config.outputDir}")
     }
 
+    /**
+     * Output executable file<br>
+     *
+     * Defaults to this task's name appended with '.exe'
+     */
     @Input
-    @Optional
-    String outfile
+    String outfile = "${name}.exe"
 
     @Override
     String getOutfile() {
-        outfile ?: config.outfile
+        outfile ? (outfile == (Launch4jPlugin.TASK_RUN_NAME + '.exe') ? config.outfile : outfile) : config.outfile
     }
 
     @Override
     @OutputFile
     File getDest() {
-        project.file("${getOutputDirectory()}/${outfile ?: config.outfile}")
+        project.file("${getOutputDirectory()}/${getOutfile()}")
     }
 
     @Input
-    @Optional
     String xmlFileName = "${name}.xml"
-//    @InputFile
+
     @Override
     File getXmlFile() {
         project.file("${getOutputDirectory()}/${xmlFileName ?: config.xmlFileName}")
     }
 
+    /**
+     * The name where the dependency jars should be copied to.<br>
+     *
+     * Defaults to <u>lib</u>
+     */
     @Input
     @Optional
     String libraryDir
@@ -86,6 +91,9 @@ abstract class DefaultLaunch4jTask extends DefaultTask implements Launch4jConfig
         new CopyLibraries(project).execute(getLibraryDirectory(), copyConfigurable ?: config.copyConfigurable)
     }
 
+    /**
+     * The main class to start if {@link #jar} is not set.
+     */
     @Input
     @Optional
     String mainClassName
@@ -94,6 +102,12 @@ abstract class DefaultLaunch4jTask extends DefaultTask implements Launch4jConfig
     String getMainClassName() {
         mainClassName ?: config.mainClassName
     }
+
+    /**
+     * Optional, by default specifies the jar to wrap.
+     * To launch a jar without wrapping it enter the runtime path of the jar relative to the executable and set {@link #dontWrapJar} to true.
+     * For example, if the executable launcher and the application jar named <i>calc.exe</i> and <i>calc.jar</i> are in the same directory then you would use {@code jar = calc.jar} and {@code dontWrapJar = true}.
+     */
     @Input
     @Optional
     String jar
@@ -103,6 +117,11 @@ abstract class DefaultLaunch4jTask extends DefaultTask implements Launch4jConfig
         jar ? internalJar() : config.internalJar()
     }
 
+    /**
+     * Optional, defaults to <u>false</u>.<br>
+     *     Launch4j by default wraps jars in native executables, you can prevent this by setting {@link #dontWrapJar} to true.
+     *     The exe acts then as a launcher and starts the application specified in {@link #jar} or this runtime dependencies and {@link #mainClassName}
+     */
     @Input
     @Optional
     Boolean dontWrapJar = false
@@ -112,6 +131,11 @@ abstract class DefaultLaunch4jTask extends DefaultTask implements Launch4jConfig
         dontWrapJar ?: config.dontWrapJar
     }
 
+    /**
+     * Type of the header used to wrap the application.
+     * <br>
+     * Must be one of <u>gui</u>, console, jniGui32, jniConsole32
+     */
     @Input
     @Optional
     String headerType
@@ -121,6 +145,10 @@ abstract class DefaultLaunch4jTask extends DefaultTask implements Launch4jConfig
         headerType ?: config.headerType
     }
 
+    /**
+     * Optional, sets the title of the error message box that's displayed if Java cannot be found for instance.
+     * This usually should contain the name of your application. The console header prefixes error messages with this property (myapp: error...)
+     */
     @Input
     @Optional
     String errTitle
@@ -130,6 +158,9 @@ abstract class DefaultLaunch4jTask extends DefaultTask implements Launch4jConfig
         errTitle ?: config.errTitle
     }
 
+    /**
+     * Optional, constant command line arguments.
+     */
     @Input
     @Optional
     String cmdLine
@@ -139,6 +170,11 @@ abstract class DefaultLaunch4jTask extends DefaultTask implements Launch4jConfig
         cmdLine ?: config.cmdLine
     }
 
+    /**
+     * Optional. Change current directory to an arbitrary path relative to the executable.
+     * If you omit this property or leave it blank it will have no effect.
+     * Setting it to {@code .} will change the current dir to the same directory as the executable. {@code ..} will change it to the parent directory, and so on.
+     */
     @Input
     @Optional
     String chdir
@@ -148,6 +184,10 @@ abstract class DefaultLaunch4jTask extends DefaultTask implements Launch4jConfig
         chdir ?: config.chdir
     }
 
+    /**
+     * The process priority.<br>
+     * Can be <u>{@code normal}</u>, {@code idle} or {@code high}.
+     */
     @Input
     @Optional
     String priority
@@ -157,6 +197,11 @@ abstract class DefaultLaunch4jTask extends DefaultTask implements Launch4jConfig
         priority ?: config.priority
     }
 
+    /**
+     * The page to navigate the browser to if no java environment is found.<br>
+     *
+     * Defaults to <u>http://java.com/download</u>
+     */
     @Input
     @Optional
     String downloadUrl
@@ -175,6 +220,9 @@ abstract class DefaultLaunch4jTask extends DefaultTask implements Launch4jConfig
         supportUrl ?: config.supportUrl
     }
 
+    /**
+     * Optional, defaults to <u>false</u> in GUI header, always true in console header. When enabled the launcher waits for the Java application to finish and returns it's exit code.
+     */
     @Input
     @Optional
     Boolean stayAlive
@@ -184,6 +232,10 @@ abstract class DefaultLaunch4jTask extends DefaultTask implements Launch4jConfig
         stayAlive ?: config.stayAlive
     }
 
+    /**
+     * Optional, defaults to <u>false</u>.
+     * When the application exits, any exit code other than 0 is considered a crash and the application will be started again.
+     */
     @Input
     @Optional
     Boolean restartOnCrash
@@ -202,6 +254,9 @@ abstract class DefaultLaunch4jTask extends DefaultTask implements Launch4jConfig
         manifest ?: config.manifest
     }
 
+    /**
+     * Application icon in ICO format. May contain multiple color depths/resolutions.
+     */
     @Input
     @Optional
     String icon
@@ -211,6 +266,9 @@ abstract class DefaultLaunch4jTask extends DefaultTask implements Launch4jConfig
         icon ?: config.icon
     }
 
+    /**
+     * Version number 'x.x.x.x'
+     */
     @Input
     @Optional
     String version
@@ -220,6 +278,9 @@ abstract class DefaultLaunch4jTask extends DefaultTask implements Launch4jConfig
         version ?: config.version
     }
 
+    /**
+     * Free form file version, for example '1.20.RC1'.
+     */
     @Input
     @Optional
     String textVersion
@@ -229,6 +290,9 @@ abstract class DefaultLaunch4jTask extends DefaultTask implements Launch4jConfig
         textVersion ?: config.textVersion
     }
 
+    /**
+     * Legal copyright.
+     */
     @Input
     @Optional
     String copyright
@@ -238,6 +302,9 @@ abstract class DefaultLaunch4jTask extends DefaultTask implements Launch4jConfig
         copyright ?: config.copyright
     }
 
+    /**
+     * Optional, accepts everything you would normally pass to java/javaw launcher: assertion options, system properties and X options. Here you can map environment and special variables <i>EXEDIR</i> (exe's runtime directory), <i>EXEFILE</i> (exe's runtime full file path) to system properties. All variable references must be surrounded with percentage signs and quoted.
+     */
     @Input
     @Optional
     String opt
@@ -247,6 +314,9 @@ abstract class DefaultLaunch4jTask extends DefaultTask implements Launch4jConfig
         opt ?: config.opt
     }
 
+    /**
+     * Optional text.
+     */
     @Input
     @Optional
     String companyName
@@ -256,6 +326,9 @@ abstract class DefaultLaunch4jTask extends DefaultTask implements Launch4jConfig
         companyName ?: config.companyName
     }
 
+    /**
+     * File description presented to the user.
+     */
     @Input
     @Optional
     String fileDescription
@@ -265,6 +338,9 @@ abstract class DefaultLaunch4jTask extends DefaultTask implements Launch4jConfig
         fileDescription ?: config.fileDescription
     }
 
+    /**
+     * Text.
+     */
     @Input
     @Optional
     String productName
@@ -274,6 +350,9 @@ abstract class DefaultLaunch4jTask extends DefaultTask implements Launch4jConfig
         productName ?: config.productName
     }
 
+    /**
+     * Internal name without extension, original filename or module name for example.
+     */
     @Input
     @Optional
     String internalName
@@ -283,6 +362,9 @@ abstract class DefaultLaunch4jTask extends DefaultTask implements Launch4jConfig
         internalName ?: config.internalName
     }
 
+    /**
+     * Trademarks that apply to the file.
+     */
     @Input
     @Optional
     String trademarks
@@ -292,6 +374,9 @@ abstract class DefaultLaunch4jTask extends DefaultTask implements Launch4jConfig
         trademarks ?: config.trademarks
     }
 
+    /**
+     * One of the language codes.
+     */
     @Input
     @Optional
     String language
@@ -300,6 +385,9 @@ abstract class DefaultLaunch4jTask extends DefaultTask implements Launch4jConfig
     String getLanguage() {
         language ?: config.language
     }
+    /**
+     * Run if bundled JRE and javaw.exe are present, otherwise stop with error
+     */
     @Input
     @Optional
     String bundledJrePath
@@ -308,6 +396,10 @@ abstract class DefaultLaunch4jTask extends DefaultTask implements Launch4jConfig
     String getBundledJrePath() {
         bundledJrePath ?: config.bundledJrePath
     }
+
+    /**
+     * Optional, defaults to <u>false</u> which limits the calculated heap size to the 32-bit maximum. Set to true in order to use the available memory without this limit. This option works in combination with the HeapSize and HeapPercent options only if the found JRE is a bundled one. In the standard JRE search based on registry the wrapper detects the type of JRE itself and uses the 32-bit heap limit when needed.
+     */
     @Input
     @Optional
     Boolean bundledJre64Bit
@@ -316,6 +408,10 @@ abstract class DefaultLaunch4jTask extends DefaultTask implements Launch4jConfig
     Boolean getBundledJre64Bit() {
         bundledJre64Bit ?: config.bundledJre64Bit
     }
+
+    /**
+     * Optional, defaults to <u>false</u> which treats the bundled JRE as the primary runtime. When set to true, the bundled JRE will only be used in case the mix/max version search fails. This can be used as a fallback option if the user does not have the required Java installed and the bundled JRE is provided on a CD or shared network location.
+     */
     @Input
     @Optional
     Boolean bundledJreAsFallback
@@ -324,6 +420,13 @@ abstract class DefaultLaunch4jTask extends DefaultTask implements Launch4jConfig
     Boolean getBundledJreAsFallback() {
         bundledJreAsFallback ?: config.bundledJreAsFallback
     }
+    /**
+     * If {@link #bundledJrePath} is set:
+     * <ul><li>Search for Java, if an appropriate version cannot be found display error message and open the Java download page.</li</ul>
+     * Else:
+     * <ul><li>Use bundled JRE first, if it cannot be located search for Java, if that fails display error message and open the Java download page. </li></ul>
+     * @see DefaultLaunch4jTask#jreMaxVersion
+     */
     @Input
     @Optional
     String jreMinVersion
@@ -345,6 +448,14 @@ abstract class DefaultLaunch4jTask extends DefaultTask implements Launch4jConfig
         }
         jreMinVersion
     }
+
+    /**
+     * If {@link #bundledJrePath} is set:
+     * <ul><li>Search for Java, if an appropriate version cannot be found display error message and open the Java download page.</li</ul>
+     * Else:
+     * <ul><li>Use bundled JRE first, if it cannot be located search for Java, if that fails display error message and open the Java download page. </li></ul>
+     * @see DefaultLaunch4jTask#jreMinVersion
+     */
     @Input
     @Optional
     String jreMaxVersion
@@ -353,6 +464,17 @@ abstract class DefaultLaunch4jTask extends DefaultTask implements Launch4jConfig
     String getJreMaxVersion() {
         jreMaxVersion ?: config.jreMaxVersion
     }
+
+    /**
+     * Optional, defaults to preferJre; Allows you to specify a preference for a public JRE or a private JDK runtime. Valid values are:
+     * <ul>
+     *     <li><strong>jreOnly</strong><br>Always use a public JRE (equivalent to the old option dontUsePrivateJres=true)</li>
+     *     <li><strong><u>preferJre</u></strong><br>Prefer a public JRE, but use a JDK private runtime if it is newer than the public JRE (equivalent to the old option dontUsePrivateJres=false)</li>
+     *     <li><strong>preferJdk</strong><br>Prefer a JDK private runtime, but use a public JRE if it is newer than the JDK </li>
+     *     <li><strong>jdkOnly</strong><br>Always use a private JDK runtime (fails if there is no JDK installed)</li>
+     * </ul>
+     *
+     */
     @Input
     @Optional
     String jdkPreference
@@ -361,6 +483,17 @@ abstract class DefaultLaunch4jTask extends DefaultTask implements Launch4jConfig
     String getJdkPreference() {
         jdkPreference ?: config.jdkPreference
     }
+
+    /**
+     * Optional, defaults to 64/32; Allows to select between 64-bit and 32-bit runtimes. Valid values are:
+     * <ul>
+     *     <li><strong>64</strong><br>Use only 64-bit runtimes</li>
+     *     <li><strong><u>64/32</u></strong><br>Use 64-bit runtimes if available, otherwise use 32-bit</li>
+     *     <li><strong>32/64</strong><br>Use 32-bit runtimes if available, otherwise use 64-bit</li>
+     *     <li><strong>32</strong><br>Use only 32-bit runtimes</li>
+     * </ul>
+     *
+     */
     @Input
     @Optional
     String jreRuntimeBits
@@ -369,6 +502,9 @@ abstract class DefaultLaunch4jTask extends DefaultTask implements Launch4jConfig
     String getJreRuntimeBits() {
         jreRuntimeBits ?: config.jreRuntimeBits
     }
+    /**
+     * Unique mutex name that will identify the application.
+     */
     @Input
     @Optional
     String mutexName
@@ -377,6 +513,10 @@ abstract class DefaultLaunch4jTask extends DefaultTask implements Launch4jConfig
     String getMutexName() {
         mutexName ?: config.mutexName
     }
+
+    /**
+     * Optional, recognized by GUI header only. Title or title part of a window to bring up instead of running a new instance.
+     */
     @Input
     @Optional
     String windowTitle
@@ -417,6 +557,15 @@ abstract class DefaultLaunch4jTask extends DefaultTask implements Launch4jConfig
     String getMessagesLauncherError() {
         messagesLauncherError ?: config.messagesLauncherError
     }
+    /**
+     * Optional, initial heap size in MB.<br>
+     *
+     * If {@link #initialHeapSize} and {@link #initialHeapPercent} are specified, then the setting which yields more memory will be chosen at runtime. In other words, setting both values means: percent of available memory no less than size in MB.
+     *
+     * @see DefaultLaunch4jTask#initialHeapPercent
+     * @see DefaultLaunch4jTask#maxHeapSize
+     * @see DefaultLaunch4jTask#maxHeapPercent
+     */
     @Input
     @Optional
     Integer initialHeapSize
@@ -425,6 +574,16 @@ abstract class DefaultLaunch4jTask extends DefaultTask implements Launch4jConfig
     Integer getInitialHeapSize() {
         initialHeapSize ?: config.initialHeapSize
     }
+
+    /**
+     * Optional, initial heap size in % of available memory.<br>
+     *
+     * If {@link #initialHeapSize} and {@link #initialHeapPercent} are specified, then the setting which yields more memory will be chosen at runtime. In other words, setting both values means: percent of available memory no less than size in MB.
+     *
+     * @see DefaultLaunch4jTask#initialHeapSize
+     * @see DefaultLaunch4jTask#maxHeapSize
+     * @see DefaultLaunch4jTask#maxHeapPercent
+     */
     @Input
     @Optional
     Integer initialHeapPercent
@@ -433,6 +592,16 @@ abstract class DefaultLaunch4jTask extends DefaultTask implements Launch4jConfig
     Integer getInitialHeapPercent() {
         initialHeapPercent ?: config.initialHeapPercent
     }
+
+    /**
+     * Optional, max heap size in MB.<br>
+     *
+     * If {@link #maxHeapSize} and {@link #maxHeapPercent} are specified, then the setting which yields more memory will be chosen at runtime. In other words, setting both values means: percent of available memory no less than size in MB.
+     *
+     * @see DefaultLaunch4jTask#initialHeapSize
+     * @see DefaultLaunch4jTask#initialHeapPercent
+     * @see DefaultLaunch4jTask#maxHeapPercent
+     */
     @Input
     @Optional
     Integer maxHeapSize
@@ -441,6 +610,16 @@ abstract class DefaultLaunch4jTask extends DefaultTask implements Launch4jConfig
     Integer getMaxHeapSize() {
         maxHeapSize ?: config.maxHeapSize
     }
+
+    /**
+     * Optional, max heap size in % of available memory.<br>
+     *
+     * If {@link #maxHeapSize} and {@link #maxHeapPercent} are specified, then the setting which yields more memory will be chosen at runtime. In other words, setting both values means: percent of available memory no less than size in MB.
+     *
+     * @see DefaultLaunch4jTask#initialHeapSize
+     * @see DefaultLaunch4jTask#initialHeapPercent
+     * @see DefaultLaunch4jTask#maxHeapSize
+     */
     @Input
     @Optional
     Integer maxHeapPercent
@@ -449,6 +628,9 @@ abstract class DefaultLaunch4jTask extends DefaultTask implements Launch4jConfig
     Integer getMaxHeapPercent() {
         maxHeapPercent ?: config.maxHeapPercent
     }
+    /**
+     * Splash screen image in BMP format.
+     */
     @Input
     @Optional
     String splashFileName
@@ -457,6 +639,10 @@ abstract class DefaultLaunch4jTask extends DefaultTask implements Launch4jConfig
     String getSplashFileName() {
         splashFileName ?: config.splashFileName
     }
+
+    /**
+     * Optional, defaults to <u>true</u>. Close the splash screen when an application window or Java error message box appears. If set to false, the splash screen will be closed on timeout.
+     */
     @Input
     @Optional
     Boolean splashWaitForWindows
@@ -465,6 +651,10 @@ abstract class DefaultLaunch4jTask extends DefaultTask implements Launch4jConfig
     Boolean getSplashWaitForWindows() {
         splashWaitForWindows ?: config.splashWaitForWindows
     }
+
+    /**
+     * Optional, defaults to <u>60</u>. Number of seconds after which the splash screen must be closed. Splash timeout may cause an error depending on {@link #splashTimeoutError}.
+     */
     @Input
     @Optional
     Integer splashTimeout
@@ -474,6 +664,9 @@ abstract class DefaultLaunch4jTask extends DefaultTask implements Launch4jConfig
         splashTimeout ?: config.splashTimeout
     }
 
+    /**
+     * Optional, defaults to <u>true</u>. True signals an error on splash timeout, false closes the splash screen quietly.
+     */
     @Input
     @Optional
     Boolean splashTimeoutError
