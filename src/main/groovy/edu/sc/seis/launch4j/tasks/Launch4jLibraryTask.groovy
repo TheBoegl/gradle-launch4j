694b83bce322373a6174991cdecbb43b08bac736
Sebastian Bögl <info@sebastianboegl.de>
2016-11-24 20:06:23 +0100

    extract launch4j libraries to buildDir/tmp/launch4j only (issue #7, #33)

--- src/main/groovy/edu/sc/seis/launch4j/tasks/Launch4jLibraryTask.groovy
+++ src/main/groovy/edu/sc/seis/launch4j/tasks/Launch4jLibraryTask.groovy
@@ -7,22 +7,27 @@ import org.gradle.api.tasks.TaskAction
 
 class Launch4jLibraryTask extends DefaultLaunch4jTask {
 
+    private static final String TEMPORARY_DIRECTORY = "tmp/launch4j"
+
     @TaskAction
     def run() {
         copyLibraries()
-        new ExtractLibraries(project).execute(getOutputDirectory())
+        def tmpDir = new File(project.buildDir, TEMPORARY_DIRECTORY)
+        new ExtractLibraries(project).execute(tmpDir)
         createXML()
         getDest().delete()
         def stdOut = new ByteArrayOutputStream()
         def execResult = project.exec {
-            commandLine "java", "-jar", "${ExtractLibraries.LAUNCH4J_BINARY_DIRECTORY}/launch4j-${Launch4jPlugin.ARTIFACT_VERSION}.jar", "${getXmlFile()}"
+            commandLine "java", "-jar", "${tmpDir}/${ExtractLibraries.LAUNCH4J_BINARY_DIRECTORY}/launch4j-${Launch4jPlugin.ARTIFACT_VERSION}.jar", "${getXmlFile()}"
             workingDir getOutputDirectory()
             standardOutput = stdOut
             errorOutput = stdOut
             ignoreExitValue = true
         }
 
-        if (execResult.exitValue != 0) {
+        if (execResult.exitValue == 0) {
+            project.delete(getXmlFile())
+        } else {
             throw new GradleException("Launch4J finished with non-zero exit value ${execResult.exitValue}\n${stdOut.toString()}");
         }
     }
