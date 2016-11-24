db61f5388bda0585c57213a95c1a7f0a406e815a
Sebastian Bögl <info@sebastianboegl.de>
2016-11-24 20:06:23 +0100

    extract launch4j binaries to temp folder

--- src/main/groovy/edu/sc/seis/launch4j/tasks/Launch4jLibraryTask.groovy
+++ src/main/groovy/edu/sc/seis/launch4j/tasks/Launch4jLibraryTask.groovy
@@ -10,19 +10,21 @@ class Launch4jLibraryTask extends DefaultLaunch4jTask {
     @TaskAction
     def run() {
         copyLibraries()
-        new ExtractLibraries(project).execute(getOutputDirectory())
+        new ExtractLibraries(project).execute(temporaryDir)
         createXML()
         getDest().delete()
         def stdOut = new ByteArrayOutputStream()
         def execResult = project.exec {
-            commandLine "java", "-jar", "${ExtractLibraries.LAUNCH4J_BINARY_DIRECTORY}/launch4j-${Launch4jPlugin.ARTIFACT_VERSION}.jar", "${getXmlFile()}"
+            commandLine "java", "-jar", "${temporaryDir}/${ExtractLibraries.LAUNCH4J_BINARY_DIRECTORY}/launch4j-${Launch4jPlugin.ARTIFACT_VERSION}.jar", "${getXmlFile()}"
             workingDir getOutputDirectory()
             standardOutput = stdOut
             errorOutput = stdOut
             ignoreExitValue = true
         }
 
-        if (execResult.exitValue != 0) {
+        if (execResult.exitValue == 0) {
+            project.delete(getXmlFile())
+        } else {
             throw new GradleException("Launch4J finished with non-zero exit value ${execResult.exitValue}\n${stdOut.toString()}");
         }
     }
