d79c04a3469b3fde73f7106b71f8556574313f2f
Sebastian Bögl <info@sebastianboegl.de>
2016-11-25 02:29:10 +0100

    configure jar path differently if it is not wrapped

--- src/test/groovy/edu/sc/seis/launch4j/Launch4jPluginExtensionTest.groovy
+++ src/test/groovy/edu/sc/seis/launch4j/Launch4jPluginExtensionTest.groovy
@@ -388,6 +388,123 @@ class Launch4jPluginExtensionTest extends Specification {
         process.err.text.trim().endsWith('Hello LOG!')
     }
 
+    def 'Running the created executable in a subfolder and default lib folder with Java dependencies succeeds'() {
+        given:
+        buildFile << """
+            plugins {
+                id 'java'
+                id 'edu.sc.seis.launch4j'
+            }
+
+            dependencies {
+                compile 'org.slf4j:slf4j-api:1.7.12'
+                runtime 'org.slf4j:slf4j-simple:1.7.12'
+            }
+
+            launch4j {
+                mainClassName = 'com.test.app.Main'
+                outfile = 'app/bin/test.exe'
+                dontWrapJar = true
+            }
+        """
+
+        File sourceFile = new File(testProjectDir.newFolder('src', 'main', 'java'), 'Main.java')
+        sourceFile << """
+            package com.test.app;
+
+            import org.slf4j.Logger;
+            import org.slf4j.LoggerFactory;
+
+            public class Main {
+                private static final Logger LOG = LoggerFactory.getLogger(Main.class);
+
+                public static void main(String[] args) {
+                    System.out.println("Hello STDOUT!");
+                    LOG.info("Hello LOG!");
+                }
+            }
+        """
+
+        when:
+        def result = GradleRunner.create()
+                .withDebug(DEBUG)
+                .withProjectDir(testProjectDir.root)
+                .withArguments('-q', 'jar', 'launch4j')
+                .withPluginClasspath(pluginClasspath)
+                .build()
+
+        then:
+        result.task(':jar').outcome == SUCCESS
+        result.task(':launch4j').outcome == SUCCESS
+
+        def outfile = new File(testProjectDir.root, 'build/launch4j/app/bin/test.exe')
+        outfile.exists()
+
+        def process = outfile.path.execute()
+        process.waitFor() == 0
+        process.in.text.trim().equals('Hello STDOUT!')
+        process.err.text.trim().endsWith('Hello LOG!')
+    }
+
+    def 'Running the created executable in a subfolder and changed lib folder with Java dependencies succeeds'() {
+        given:
+        buildFile << """
+            plugins {
+                id 'java'
+                id 'edu.sc.seis.launch4j'
+            }
+
+            dependencies {
+                compile 'org.slf4j:slf4j-api:1.7.12'
+                runtime 'org.slf4j:slf4j-simple:1.7.12'
+            }
+
+            launch4j {
+                mainClassName = 'com.test.app.Main'
+                outfile = 'app/bin/test.exe'
+                dontWrapJar = true
+                libraryDir = 'app/lib'
+            }
+        """
+
+        File sourceFile = new File(testProjectDir.newFolder('src', 'main', 'java'), 'Main.java')
+        sourceFile << """
+            package com.test.app;
+
+            import org.slf4j.Logger;
+            import org.slf4j.LoggerFactory;
+
+            public class Main {
+                private static final Logger LOG = LoggerFactory.getLogger(Main.class);
+
+                public static void main(String[] args) {
+                    System.out.println("Hello STDOUT!");
+                    LOG.info("Hello LOG!");
+                }
+            }
+        """
+
+        when:
+        def result = GradleRunner.create()
+                .withDebug(DEBUG)
+                .withProjectDir(testProjectDir.root)
+                .withArguments('-q', 'jar', 'launch4j')
+                .withPluginClasspath(pluginClasspath)
+                .build()
+
+        then:
+        result.task(':jar').outcome == SUCCESS
+        result.task(':launch4j').outcome == SUCCESS
+
+        def outfile = new File(testProjectDir.root, 'build/launch4j/app/bin/test.exe')
+        outfile.exists()
+
+        def process = outfile.path.execute()
+        process.waitFor() == 0
+        process.in.text.trim().equals('Hello STDOUT!')
+        process.err.text.trim().endsWith('Hello LOG!')
+    }
+
     def 'Running an unwrapped executable with Java dependencies succeeds'() {
         given:
         buildFile << """
