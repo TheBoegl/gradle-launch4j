TheBoegl/gradle-launch4j

commit 2d9d39d572a1ce3f6d0aa0ad1570ec13bdd88edc

migrate string property `opt` to String[] `jvmOptions` (fixes #46)
This commit deprecates the usage of the string property `opt` and introduces a new String[] property `jvmOptions`, which replaces the deprecated one internally.

src/main/groovy/edu/sc/seis/launch4j/Launch4jConfiguration.groovy
@@ -68,7 +68,15 @@ interface Launch4jConfiguration {
 
     String getCopyright()
 
-    String getOpt()
+    String[] getJvmOptions()
+
+    /**
+     * this is a backwards compatible setter for opts which was a String.
+     * @param opt the JVM options
+     * @deprecated since 2.3 use {@link #getJvmOptions()}
+     */
+    @Deprecated
+    void setOpt(String opt)
 
     String getCompanyName()
 

src/main/groovy/edu/sc/seis/launch4j/Launch4jPluginExtension.groovy
@@ -71,7 +71,15 @@ class Launch4jPluginExtension implements Launch4jConfiguration {
     String version = "${project.version}"
     String textVersion = "${project.version}"
     String copyright = 'unknown'
-    String opt = ''
+
+    String[] jvmOptions = []
+
+    @Deprecated
+    void setOpt(String opt) {
+        this.jvmOptions = [opt ]
+        project.logger.warn("${Launch4jPlugin.LAUNCH4J_EXTENSION_NAME}.opt property is deprecated. Use ${Launch4jPlugin.LAUNCH4J_EXTENSION_NAME}.jvmOptions instead.")
+    }
+
     String companyName = ''
     String fileDescription = "${project.name}"
 

src/main/groovy/edu/sc/seis/launch4j/tasks/DefaultLaunch4jTask.groovy
@@ -333,11 +333,17 @@ abstract class DefaultLaunch4jTask extends DefaultTask implements Launch4jConfig
      */
     @Input
     @Optional
-    String opt
+    String[] jvmOptions
 
     @Override
-    String getOpt() {
-        opt ?: config.opt
+    String[] getJvmOptions() {
+        jvmOptions ?: config.jvmOptions
+    }
+
+    @Deprecated
+    void setOpt(String opt) {
+        this.jvmOptions = [opt ]
+        project.logger.warn("${Launch4jPlugin.LAUNCH4J_EXTENSION_NAME}.opt property is deprecated. Use ${Launch4jPlugin.LAUNCH4J_EXTENSION_NAME}.jvmOptions instead.")
     }
 
     /**

src/main/groovy/edu/sc/seis/launch4j/CreateXML.groovy
@@ -77,7 +77,11 @@ class CreateXML {
                 xml.jdkPreference(config.jdkPreference)
                 xml.runtimeBits(config.jreRuntimeBits)
 
-                if (config.opt.length() != 0) xml.opt(config.opt)
+                config.jvmOptions.each { opt ->
+                    if (opt) {
+                        xml.opt(opt)
+                    }
+                }
 
                 if (config.initialHeapSize != null)
                     xml.initialHeapSize(config.initialHeapSize)

src/test/groovy/edu/sc/seis/launch4j/Issue46Test.groovy
@@ -1,0 +1,177 @@
+/*
+ * Copyright (c) 2017 Sebastian Boegl
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ *
+ */
+
+package edu.sc.seis.launch4j
+
+import edu.sc.seis.launch4j.util.FunctionalSpecification
+import spock.lang.Unroll
+
+import static org.gradle.testkit.runner.TaskOutcome.SUCCESS
+/**
+ * Test case to check the implementation and the backwards compatibility of the change from <tt>String opt</tt> to <tt>String[] opt</tt>.
+ */
+class Issue46Test extends FunctionalSpecification {
+
+    @Unroll
+    def 'Running the task to create the executable with the deprecated opt for language #language succeeds'() {
+        given:
+        buildFile << """
+            launch4j {
+                mainClassName = 'com.test.app.Main'
+                outfile = 'test.exe'
+                opt = "-Duser.language=${language}"
+            }
+        """
+
+        File sourceFile = new File(testProjectDir.newFolder('src', 'main', 'java'), 'Main.java')
+        sourceFile << """
+            package com.test.app;
+            
+            import java.util.Locale;
+
+            public class Main {
+                public static void main(String[] args) {
+                    if ("${language}".equals(Locale.getDefault().getLanguage())) {
+                        System.out.println("${message}");
+                    } else {
+                        System.out.println("wrong language!");
+                    }
+                }
+            }
+        """
+
+        when:
+        def result = build('createExe')
+
+        then:
+        result.task(':jar').outcome == SUCCESS
+        result.task(':createExe').outcome == SUCCESS
+
+        when:
+        def outfile = new File(projectDir, 'build/launch4j/test.exe')
+        then:
+        outfile.exists()
+
+        when:
+        def process = outfile.path.execute()
+        then:
+        process.waitFor() == 0
+        process.in.text.trim() == message
+
+        where:
+        language    | message
+        'de'        | 'Hallo Welt!'
+        'en'        | 'Hello World!'
+//        'fr'        | 'Bonjour monde!'
+//        'it'        | 'Ciao mondo!'
+    }
+
+
+    @Unroll
+    def 'Running the task to create the executable with jvmOptions for language #language succeeds'() {
+        given:
+        buildFile << """
+            launch4j {
+                mainClassName = 'com.test.app.Main'
+                outfile = 'test.exe'
+                jvmOptions = [ "-Duser.language=${language}" ]
+            }
+        """
+
+        File sourceFile = new File(testProjectDir.newFolder('src', 'main', 'java'), 'Main.java')
+        sourceFile << """
+            package com.test.app;
+            
+            import java.util.Locale;
+
+            public class Main {
+                public static void main(String[] args) {
+                    if ("${language}".equals(Locale.getDefault().getLanguage())) {
+                        System.out.println("${message}");
+                    } else {
+                        System.out.println("wrong language!");
+                    }
+                }
+            }
+        """
+
+        when:
+        def result = build('createExe')
+
+        then:
+        result.task(':jar').outcome == SUCCESS
+        result.task(':createExe').outcome == SUCCESS
+
+        when:
+        def outfile = new File(projectDir, 'build/launch4j/test.exe')
+        then:
+        outfile.exists()
+
+        when:
+        def process = outfile.path.execute()
+        then:
+        process.waitFor() == 0
+        process.in.text.trim() == message
+
+        where:
+        language    | message
+        'de'        | 'Hallo Welt!'
+        'en'        | 'Hello World!'
+//        'fr'        | 'Bonjour monde!'
+//        'it'        | 'Ciao mondo!'
+    }
+
+    def 'Running the task to create the executable without jvm options succeeds'() {
+        given:
+
+        buildFile << '''
+            launch4j {
+                mainClassName = 'com.test.app.Main'
+                outfile = 'test.exe'
+            }
+        '''
+
+        File sourceFile = new File(testProjectDir.newFolder('src', 'main', 'java'), 'Main.java')
+        sourceFile << """
+            package com.test.app;
+            
+            public class Main {
+                public static void main(String[] args) {
+                    System.out.println("Hello World!");
+                }
+            }
+        """
+
+        when:
+        def result = build('createExe')
+
+        then:
+        result.task(':jar').outcome == SUCCESS
+        result.task(':createExe').outcome == SUCCESS
+
+        when:
+        def outfile = new File(projectDir, 'build/launch4j/test.exe')
+        then:
+        outfile.exists()
+
+        when:
+        def process = outfile.path.execute()
+        then:
+        process.waitFor() == 0
+        process.in.text.trim() == 'Hello World!'
+    }
+}

commit 444d484e584167d874aeaf88d91c02f9a9659612

fix up-to-date on jvmOptions (issue #46)
This bug was introduced with 2d9d39d.

src/main/groovy/edu/sc/seis/launch4j/Launch4jConfiguration.groovy
@@ -68,7 +68,7 @@ interface Launch4jConfiguration {
 
     String getCopyright()
 
-    String[] getJvmOptions()
+    Set<String> getJvmOptions()
 
     /**
      * this is a backwards compatible setter for opts which was a String.

src/main/groovy/edu/sc/seis/launch4j/Launch4jPluginExtension.groovy
@@ -72,11 +72,12 @@ class Launch4jPluginExtension implements Launch4jConfiguration {
     String textVersion = "${project.version}"
     String copyright = 'unknown'
 
-    String[] jvmOptions = []
+    Set<String> jvmOptions = []
 
     @Deprecated
     void setOpt(String opt) {
-        this.jvmOptions = [opt ]
+        if (!opt) return // null check
+        this.jvmOptions = [ opt ] as Set
         project.logger.warn("${Launch4jPlugin.LAUNCH4J_EXTENSION_NAME}.opt property is deprecated. Use ${Launch4jPlugin.LAUNCH4J_EXTENSION_NAME}.jvmOptions instead.")
     }
 

src/main/groovy/edu/sc/seis/launch4j/tasks/DefaultLaunch4jTask.groovy
@@ -334,16 +334,17 @@ abstract class DefaultLaunch4jTask extends DefaultTask implements Launch4jConfig
      */
     @Input
     @Optional
-    String[] jvmOptions
+    Set<String> jvmOptions = []
 
     @Override
-    String[] getJvmOptions() {
+    Set<String> getJvmOptions() {
         jvmOptions ?: config.jvmOptions
     }
 
     @Deprecated
     void setOpt(String opt) {
-        this.jvmOptions = [opt ]
+        if (!opt) return // null check
+        this.jvmOptions = [ opt ] as Set
         project.logger.warn("${Launch4jPlugin.LAUNCH4J_EXTENSION_NAME}.opt property is deprecated. Use ${Launch4jPlugin.LAUNCH4J_EXTENSION_NAME}.jvmOptions instead.")
     }
 

src/test/groovy/edu/sc/seis/launch4j/Launch4jPluginExtensionTest.groovy
@@ -22,6 +22,7 @@ import org.gradle.util.GradleVersion
 import spock.lang.Unroll
 
 import static org.gradle.testkit.runner.TaskOutcome.SUCCESS
+import static org.gradle.testkit.runner.TaskOutcome.UP_TO_DATE
 
 class Launch4jPluginExtensionTest extends FunctionalSpecification {
 
@@ -166,6 +167,44 @@ class Launch4jPluginExtensionTest extends FunctionalSpecification {
         !new File(projectDir, 'build/launch4j/Test.exe').exists()
         !new File(projectDir, 'build/launch4j/lib/testProject.jar').exists()
         !new File(projectDir, 'build/launch4j').exists()
+    }
+
+    def 'Running the task to create the executable and be UP-To-DATE succeeds'() {
+        given:
+        buildFile << """
+            launch4j {
+                mainClassName = 'com.test.app.Main'
+                outfile = 'Test.exe'
+            }
+        """
+        testProjectDir.newFile('settings.gradle').text = "rootProject.name = 'testProject'"
+
+        File sourceFile = new File(testProjectDir.newFolder('src', 'main', 'java'), 'Main.java')
+        sourceFile << """
+            package com.test.app;
+
+            public class Main {
+                public static void main(String[] args) {
+                    System.out.println("Hello World!");
+                }
+            }
+        """
+
+        when:
+        def result = build('createExe')
+
+        then:
+        result.task(':jar').outcome == SUCCESS
+        result.task(':createExe').outcome == SUCCESS
+
+        new File(projectDir, 'build/launch4j/Test.exe').exists()
+        new File(projectDir, 'build/launch4j/lib/testProject.jar').exists()
+
+        when:
+        def resultTwo = build('createExe')
+
+        then:
+        resultTwo.task(':createExe').outcome == UP_TO_DATE
     }
 
     def 'Running the library task to create the executable succeeds'() {