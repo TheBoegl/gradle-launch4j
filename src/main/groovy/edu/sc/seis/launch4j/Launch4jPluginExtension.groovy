502f4b9dd945d86d5338bdf16e531e04cf72b521
Sebastian Bögl <info@sebastianboegl.de>
2019-01-20 22:23:22 +0100

    :arrow_up: gradle 4.10.3
    use latest shadow plugin which is compatible with gradle 4.+

--- src/main/groovy/edu/sc/seis/launch4j/Launch4jPluginExtension.groovy
+++ src/main/groovy/edu/sc/seis/launch4j/Launch4jPluginExtension.groovy
@@ -27,7 +27,7 @@ import org.gradle.api.tasks.Input
 import org.gradle.api.tasks.bundling.Jar
 
 @CompileStatic
-@AutoClone(excludes = [''])
+@AutoClone
 class Launch4jPluginExtension implements Launch4jConfiguration {
 
     private final Project project
