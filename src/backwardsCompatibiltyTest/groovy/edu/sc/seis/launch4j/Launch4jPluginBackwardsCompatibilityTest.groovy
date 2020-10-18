3fd80db9306c941933375ead585a3c1e728d245b
Sebastian Bögl <info@sebastianboegl.de>
2020-10-18 23:09:20 +0200

    run backwards compatible tests for every last mayor gradle version

--- src/backwardsCompatibiltyTest/groovy/edu/sc/seis/launch4j/Launch4jPluginBackwardsCompatibilityTest.groovy
+++ src/backwardsCompatibiltyTest/groovy/edu/sc/seis/launch4j/Launch4jPluginBackwardsCompatibilityTest.groovy
@@ -55,6 +55,6 @@ class Launch4jPluginBackwardsCompatibilityTest extends FunctionalSpecification {
 
         where:
         // versions prior 2.8 will not allow the classpath injection
-        gradleVersion << ['2.14', '2.14.1', '3.0', '3.2.1', '3.3', '3.4.1', '4.10.2', GradleVersion.current().getVersion()]
+        gradleVersion << ['2.14.1', '3.5.1', '4.10.2', '5.6.4', GradleVersion.current().getVersion()]
     }
 }
