a45317864476f0d59a35f9d3104fa0d19a7e169c
Sebastian Bögl <info@sebastianboegl.de>
2023-05-04 00:16:40 +0200

    add a test with toolchain support
    This commit adds another test case for issue #88 which uses the java toolchain that was introduced with gradle 6.7.

--- src/test/groovy/edu/sc/seis/launch4j/Issue88Test.groovy
+++ src/test/groovy/edu/sc/seis/launch4j/Issue88Test.groovy
@@ -33,6 +33,38 @@ class Issue88Test extends FunctionalSpecification {
         xml.contains('<minVersion>1.7.0</minVersion>')
     }
 
+    def 'verify toolchain is used as minimum version'() {
+        given:
+        buildFile << """
+            java {
+                toolchain {
+                    languageVersion = JavaLanguageVersion.of(8) // use same version as the one building and provided below
+                }
+            }
+            launch4j {
+                outfile = 'test.exe'
+            }
+        """
+
+        when:
+        // disable auto-detect and put the current jdk on the search path as this fails otherwise
+        def result = createAndConfigureGradleRunner('createExe', '-Pl4j-debug', '-Porg.gradle.java.installations.auto-detect=false', '-Porg.gradle.java.installations.paths=' + System.getProperty("java.home")).build()
+
+        then:
+        result.task(':jar').outcome == SUCCESS
+        result.task(':createExe').outcome == SUCCESS
+
+        when:
+        def xmlFile = new File(projectDir, 'build/tmp/createExe/createExe.xml')
+        then:
+        xmlFile.exists()
+
+        when:
+        def xml = xmlFile.text
+        then:
+        xml.contains('<minVersion>1.8.0</minVersion>')
+    }
+
     def 'verify source compatibility in java plugin is used as minimum version'() {
         given:
         buildFile << """
