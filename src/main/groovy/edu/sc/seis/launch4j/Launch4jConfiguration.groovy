27d61f507b8bd02e0ea1237ee2dced5c1cee72a3
Sebastian Bögl <info@sebastianboegl.de>
2025-07-26 18:48:56 +0200

    simplify consuming this plugin
    This commit fixes #182 by defining non-conflicting outputs.

--- src/main/groovy/edu/sc/seis/launch4j/Launch4jConfiguration.groovy
+++ src/main/groovy/edu/sc/seis/launch4j/Launch4jConfiguration.groovy
@@ -1,5 +1,5 @@
 /*
- * Copyright (c) 2024 Sebastian Boegl
+ * Copyright (c) 2025 Sebastian Boegl
  *
  * Licensed under the Apache License, Version 2.0 (the "License");
  * you may not use this file except in compliance with the License.
@@ -39,7 +39,7 @@ interface Launch4jConfiguration {
     @OutputDirectory
     DirectoryProperty getOutputDirectory()
 
-    @OutputFile
+    @Internal
     Provider<RegularFile> getDest()
 
     @Internal("usually the xml file is deleted anyways")
--- src/main/groovy/edu/sc/seis/launch4j/tasks/DefaultLaunch4jTask.groovy
+++ src/main/groovy/edu/sc/seis/launch4j/tasks/DefaultLaunch4jTask.groovy
@@ -1,5 +1,5 @@
 /*
- * Copyright (c) 2024 Sebastian Boegl
+ * Copyright (c) 2025 Sebastian Boegl
  *
  * Licensed under the Apache License, Version 2.0 (the "License");
  * you may not use this file except in compliance with the License.
@@ -274,7 +274,7 @@ abstract class DefaultLaunch4jTask extends DefaultTask implements Launch4jConfig
     @Input
     final Property<String> outfile
 
-    @OutputFile
+    @Internal
     final Provider<RegularFile> dest
 
     @Input
